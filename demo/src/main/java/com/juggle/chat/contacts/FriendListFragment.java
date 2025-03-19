14a12fbec0ecfd51df05553d5644617e6595c88c
Nathan <14392240@qq.com>
2025-03-19 16:25:02 +0800

    update header of contacts

--- demo/src/main/java/com/juggle/chat/contacts/FriendListFragment.java
+++ demo/src/main/java/com/juggle/chat/contacts/FriendListFragment.java
@@ -1,5 +1,7 @@
 package com.juggle.chat.contacts;
 
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +20,7 @@ import com.juggle.chat.bean.HttpResult;
 import com.juggle.chat.bean.ListResult;
 import com.juggle.chat.contacts.chatroom.ChatroomListActivity;
 import com.juggle.chat.contacts.group.GroupListActivity;
+import com.juggle.chat.contacts.group.select.SelectGroupMemberActivity;
 import com.juggle.chat.databinding.FragmentFriendsGroupsBinding;
 import com.juggle.chat.contacts.add.AddFriendListActivity;
 import com.juggle.chat.http.CustomCallback;
@@ -31,31 +34,30 @@ import com.juggle.im.model.Conversation;
  */
 public class FriendListFragment extends Fragment {
     private FragmentFriendsGroupsBinding binding;
-    private final StateHeaderComponent headerComponent = new StateHeaderComponent();
     private final FriendAdapter adapter = new FriendAdapter();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentFriendsGroupsBinding.inflate(inflater, container, false);
-        headerComponent.getParams().setTitle(getString(R.string.text_tab_friends));
-        headerComponent.getParams().setUseLeftButton(false);
-        headerComponent.getParams().setRightButtonText("Add");
-        headerComponent.setOnRightButtonClickListener(v -> startActivity(AddFriendListActivity.newIntent(getContext())));
-        View header = headerComponent.onCreateView(requireContext(), inflater, binding.headerComponent, savedInstanceState);
-        binding.headerComponent.addView(header);
-        adapter.setOnItemClickListener(new OnItemClickListener<FriendBean>() {
-            @Override
-            public void onItemClick(@NonNull View view, int position, @NonNull FriendBean data) {
-                if (position == 0) {
 
-                } else if (position == 1) {
-                    startActivity(GroupListActivity.newIntent(requireContext()));
-                } else if (position == 2) {
-                    startActivity(ChatroomListActivity.newIntent(requireContext()));
-                } else {
-                    startActivity(ChannelActivity.newIntent(requireContext(), Conversation.ConversationType.PRIVATE.getValue(), data.getUser_id()));
-                }
+        binding.headerView.getTitleTextView().setText(getString(R.string.text_tab_friends));
+        binding.headerView.getLeftButton().setVisibility(View.GONE);
+        binding.headerView.setRightButtonImageResource(com.jet.im.kit.R.drawable.icon_create);
+        binding.headerView.setRightButtonTint(SendbirdUIKit.getDefaultThemeMode().getPrimaryTintColorStateList(getContext()));
+        binding.headerView.setOnRightButtonClickListener(v -> {
+            startActivity(AddFriendListActivity.newIntent(getContext()));
+        });
+
+        adapter.setOnItemClickListener((view, position, data) -> {
+            if (position == 0) {
+
+            } else if (position == 1) {
+                startActivity(GroupListActivity.newIntent(requireContext()));
+            } else if (position == 2) {
+                startActivity(ChatroomListActivity.newIntent(requireContext()));
+            } else {
+                startActivity(ChannelActivity.newIntent(requireContext(), Conversation.ConversationType.PRIVATE.getValue(), data.getUser_id()));
             }
         });
 
